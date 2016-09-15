160b8a37b52461041b1f55aa8771202a51b5bce9
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2016-09-15 12:45:33 +0100

    Initial stab at transpiling Nunjucks to ERB
    Build a Gulp plugin that will do the transpilation. A lot of this will need to be factored out into a core transpiler when we add more targets.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp')
+var del = require('del')
 
 // Config for paths
 paths = {
   assets: 'app/assets/',
   assets_scss: 'app/assets/scss/',
   dist: 'dist/',
-  templates: 'app/templates',
+  templates: 'app/templates/',
   npm: 'node_modules/',
   prototype_scss: 'node_modules/@gemmaleigh/prototype-scss-refactor/**/*.scss' // This can be removed later
 }
@@ -16,4 +17,16 @@ paths = {
 gulp.task('copy:prototype-scss-refactor', function () {
   gulp.src(paths.prototype_scss)
     .pipe(gulp.dest(paths.assets_scss))
+})
+
+gulp.task('dist:del', function () {
+  return del([paths.dist + '*'])
+})
+
+gulp.task('transpile:erb', function () {
+  let erbTranspiler = require('./lib/transpilation/erb_transpiler.js')
+
+  return gulp.src(paths.templates + 'govuk_template.html')
+    .pipe(erbTranspiler())
+    .pipe(gulp.dest(paths.dist))
 })
