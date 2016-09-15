76e8339a87eaa9217e2b2ec9e527fd21458633b6
Gemma Leigh <hello@gemmaleigh.com>
2016-09-15 09:12:04 +0100

    Add the prototype scss refactor package
    This can be removed later, for now it is copying the prototype scss
    refactor work to app/assets/scss - later these files will live in this
    repo, so this copy step can be removed.
    
    Also ignore these files in .git.

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,11 @@ paths = {
   npm: 'node_modules/',
   prototype_scss: 'node_modules/@gemmaleigh/prototype-scss-refactor/**/*.scss' // This can be removed later
 }
+
+// Setup tasks
+// Copy prototype-scss-refactor into app/assets/scss
+// This step can be removed later once these files exist here
+gulp.task('copy:prototype-scss-refactor', function () {
+  gulp.src(paths.prototype_scss)
+    .pipe(gulp.dest(paths.assets_scss))
+})
