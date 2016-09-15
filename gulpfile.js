7625a7917e461e33667f232e712ac15ed59d9705
Gemma Leigh <hello@gemmaleigh.com>
2016-09-15 09:10:09 +0100

    Add paths config

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,11 @@
 var gulp = require('gulp')
+
+// Config for paths
+paths = {
+  assets: 'app/assets/',
+  assets_scss: 'app/assets/scss/',
+  dist: 'dist/',
+  templates: 'app/templates',
+  npm: 'node_modules/',
+  prototype_scss: 'node_modules/@gemmaleigh/prototype-scss-refactor/**/*.scss' // This can be removed later
+}
