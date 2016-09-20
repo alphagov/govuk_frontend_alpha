dad356b7830185f7c18d8a3357a66d522062d96b
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2016-09-20 23:48:34 +0100

    Add a generic gulp transpile task
    We can refactor the shared code between different transpilation recipes out into a separate function.

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,13 @@ var paths = {
   prototype_scss: 'node_modules/@gemmaleigh/prototype-scss-refactor/**/*.scss' // This can be removed later
 }
 
+var transpileRunner = function (templateLanguage) {
+  return gulp.src(paths.templates + 'govuk_template.html')
+    .pipe(transpiler(templateLanguage))
+    .pipe(rename({extname: '.html.' + templateLanguage}))
+    .pipe(gulp.dest(paths.dist))
+}
+
 // Setup tasks
 // Copy prototype-scss-refactor into app/assets/scss
 // This step can be removed later once these files exist here
@@ -27,18 +34,6 @@ gulp.task('clean', function () {
   return del([paths.dist + '*'])
 })
 
-gulp.task('transpile:erb', function () {
-  var templateLanguage = 'erb'
-  return gulp.src(paths.templates + 'govuk_template.html')
-    .pipe(transpiler(templateLanguage))
-    .pipe(rename({extname: '.html.' + templateLanguage}))
-    .pipe(gulp.dest(paths.dist))
-})
-
-gulp.task('transpile:handlebars', function () {
-  var templateLanguage = 'handlebars'
-  return gulp.src(paths.templates + 'govuk_template.html')
-    .pipe(transpiler(templateLanguage))
-    .pipe(rename({extname: '.html.' + templateLanguage}))
-    .pipe(gulp.dest(paths.dist))
-})
+gulp.task('transpile', ['transpile:erb', 'transpile:handlebars'])
+gulp.task('transpile:erb', transpileRunner.bind(null, 'erb'))
+gulp.task('transpile:handlebars', transpileRunner.bind(null, 'handlebars'))
