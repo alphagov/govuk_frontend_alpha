34c724982393b79408fcaaff9213cea676a381e0
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2016-09-22 15:47:11 +0100

    Tidy up the gulpfile a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var gulp = require('gulp')
-var del = require('del')
-var rename = require('gulp-rename')
-var transpiler = require('./lib/transpilation/transpiler.js')
+const gulp = require('gulp')
+const del = require('del')
+const rename = require('gulp-rename')
+const transpiler = require('./lib/transpilation/transpiler.js')
 
 // Config for paths
-var paths = {
+const paths = {
   assets: 'app/assets/',
   assets_scss: 'app/assets/scss/',
   dist: 'dist/',
@@ -15,27 +15,26 @@ var paths = {
   prototype_scss: 'node_modules/@gemmaleigh/prototype-scss-refactor/**/*.scss' // This can be removed later
 }
 
-var transpileRunner = function (templateLanguage) {
+// Task for cleaning the distribution
+gulp.task('clean', () => {
+  return del([paths.dist + '*'])
+})
+
+// Task for copying the assets
+gulp.task('copy:prototype-scss-refactor', () => {
+  gulp.src(paths.prototype_scss)
+    .pipe(gulp.dest(paths.assets_scss))
+})
+
+// Task for transpiling the templates
+let transpileRunner = templateLanguage => {
   return gulp.src(paths.templates + 'govuk_template.html')
     .pipe(transpiler(templateLanguage))
     .pipe(rename({extname: '.html.' + templateLanguage}))
     .pipe(gulp.dest(paths.dist))
 }
-
-// Setup tasks
-// Copy prototype-scss-refactor into app/assets/scss
-// This step can be removed later once these files exist here
-gulp.task('copy:prototype-scss-refactor', function () {
-  gulp.src(paths.prototype_scss)
-    .pipe(gulp.dest(paths.assets_scss))
-})
-
-gulp.task('clean', function () {
-  return del([paths.dist + '*'])
-})
-
 gulp.task('transpile', ['transpile:nunjucks', 'transpile:erb', 'transpile:handlebars', 'transpile:django'])
-gulp.task('transpile:nunjucks', function () {
+gulp.task('transpile:nunjucks', () => {
   return gulp.src(paths.templates + 'govuk_template.html')
     .pipe(rename({extname: '.html.nunjucks'}))
     .pipe(gulp.dest(paths.dist))
