d1e2bcafdacede206d0951660f5f9a33e24e641b
Gemma Leigh <hello@gemmaleigh.com>
2016-10-31 20:17:58 +0000

    Add Express JS, Nunjucks and render the govuk_template
    Use `npm start` to run the app.
    This will run `gulp preview` to ensure assets are copied to /public before `node server.js` is
    used to start the app/.
    
    Set up a simple express js app and render a test view that imports the govuk_template.html.

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const nunjucks = require('nunjucks')
+
+// Set up App
+const appViews = [
+  path.join(__dirname, '/app/views/'),
+  path.join(__dirname, '/app/templates/')
+]
+
+nunjucks.configure(appViews, {
+  autoescape: true,
+  express: app,
+  noCache: true,
+  watch: true
+})
+
+// Set views engine
+app.set('view engine', 'html')
+
+// Serve static content for the app from the "public" directory
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+// Send assetPath to all views
+app.use(function (req, res, next) {
+  res.locals.asset_path = '/public/'
+  next()
+})
+
+// Render views/index
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+// Log when app is running
+app.listen(3000, function () {
+  console.log('App is running...')
+})
