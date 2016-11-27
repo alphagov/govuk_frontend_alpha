e9412affede214874ac07c398a4adcd1eef91de6
David Singleton <david@dsingleton.co.uk>
2016-11-27 11:46:19 +0000

    Adds smoketest for components using example data
    Try and render each component using the default context from it's
    data file, ensuring no errors or thrown. Adds additional testing
    confidence automatically.
    
    Some helper duplication with button spec, to be refactored in the
    future.

---
+++ test/specs/components/smoke_spec.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect
+const nunjucks = require('nunjucks')
+const components = require('../../../lib/components')
+
+const expectComponentRenders = (name, input) => {
+  let componentPath = components.templatePathFor(name)
+  expect(() => {
+    nunjucks.render(componentPath, input)
+  }).to.not.throw()
+}
+
+describe('Components render examples without errors', function () {
+  Object.keys(components.all).map(name => {
+    it(`${name} component renders all variants without errors`, () => {
+      let component = components.get(name)
+      let variants = [component.context].concat(component.variants)
+      for (let variant of variants) {
+        expectComponentRenders(name, variant)
+      }
+    })
+  })
+})
