43e55ae5b9a526789586c8c5e1b123c999b6569f
Gemma Leigh <hello@gemmaleigh.com>
2016-12-02 14:54:18 +0000

    Remove all preview functionality and update tests
    Tests:
    
    Update the Fractal test to check assets have been copied
    
    Remove the preview test as this isn’t being used (and is also causing
    the test suite to exit before it reaches the test:fractal task.
    
    Remove preview functionality:
    
    Remove the browser-sync, start server and watch tasks.
    These tasks now exist as gulp tasks for Fractal, or are handled by
    Fractal.

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ exports.packageName = packageJson.name + '-' + packageJson.version
 
 // Gulp utility
 const gulp = require('gulp')
-const browserSync = require('browser-sync')
 const del = require('del')
 const runSequence = require('run-sequence')
 const taskListing = require('gulp-task-listing')
@@ -30,11 +29,6 @@ require('./lib/tasks/fractal-assets.js')
 require('./lib/tasks/fractal-build.js')
 require('./lib/tasks/fractal-watch.js')
 
-require('./lib/tasks/preview.js')
-require('./lib/tasks/browser-sync.js')
-require('./lib/tasks/watch.js')
-require('./lib/tasks/start-server.js')
-
 // Run 'gulp help' to list available tasks
 gulp.task('help', taskListing.withFilters(null, 'help'))
 
@@ -51,20 +45,13 @@ gulp.task('build', cb => {
 gulp.task('lint', ['lint:styles', 'lint:scripts', 'lint:tests'])
 
 // Task to run the tests
-// This runs build before testing the preview task, to copy assets to dist/bundle and /public
 gulp.task('test', cb => {
-  runSequence('lint', 'test:lib', 'test:toolkit', 'build', 'test:preview', cb)
+  runSequence('lint', 'test:lib', 'test:toolkit', 'fractal:test', cb)
 })
 
 // Package the contents of dist
 gulp.task('package', cb => {
   runSequence('build', ['package:gem', 'package:npm'], cb)
-})
-
-// Preview
-// This runs the build task first, watches and starts the server
-gulp.task('preview', cb => {
-  runSequence('build', 'start:server', ['browser-sync', 'watch'], cb)
 })
 
 // Fractal
---
+++ test/specs/fractal_spec.js
@@ -0,0 +1,31 @@
+/* eslint-env mocha */
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+
+// Check that everything is in place for Fractal to run
+
+// Check the build tasks has copied the assets to public
+describe('the build task', function () {
+  it('should copy assets into the /public directory', function () {
+    assert.doesNotThrow(function () {
+      fs.accessSync(path.resolve(__dirname, '../../public/javascripts/elements.js'))
+      fs.accessSync(path.resolve(__dirname, '../../public/javascripts/govuk-template.js'))
+      fs.accessSync(path.resolve(__dirname, '../../public/javascripts/toolkit.js'))
+      fs.accessSync(path.resolve(__dirname, '../../public/images/crests/bis_crest_13px.png'))
+      fs.accessSync(path.resolve(__dirname, '../../public/images/template/apple-touch-icon-120x120.png'))
+      fs.accessSync(path.resolve(__dirname, '../../public/images/toolkit/accordion-arrow-2x.png'))
+      fs.accessSync(path.resolve(__dirname, '../../public/stylesheets/govuk-template-print.css'))
+      fs.accessSync(path.resolve(__dirname, '../../public/stylesheets/govuk-frontend.css'))
+    })
+  })
+})
+
+// Check the theme tasks has compiled the theme stylesheet
+describe('the theme task', function () {
+  it('should compile the sass to css in the /fractal/govuk-theme directory', function () {
+    assert.doesNotThrow(function () {
+      fs.accessSync(path.resolve(__dirname, '../../fractal/govuk-theme/assets/css/fractal-govuk-theme.css'))
+    })
+  })
+})
