b3c649f24947ff13ffeec3fa69ad5cf9e2719d60
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-12-09 16:50:25 +0000

    Add component escaping spec example
    Assert that values are made html safe by default, which is the default
    behaviour for `nunjucks`, see:
    
    https://mozilla.github.io/nunjucks/api.html#autoescaping
    
    This behaviour will need to be mirrored across all supported templating
    languages, if thats not possible we might be able to change behaviour
    of meta-template for the output formatter for those formats, or if
    thats not possible/too complicated, invert the behaviour in base
    templates and have auto-escape off, and manually escape each item used,
    which would be more widely supported, but much more likely to introduce
    a security bug.

--- test/specs/components/form_group_spec.js
+++ test/specs/components/form_group_spec.js
@@ -36,6 +36,24 @@ describe('Form group component', function () {
     )
   })
 
+  it('should render with an escaped unsafe value', () => {
+    expectComponent(
+      'form-group',
+      {
+        'id': 'full-name',
+        'name': 'full-name',
+        'label': 'Full name',
+        'value': '"/><script>document.location.href="http://example.com"</script>'
+      },
+      `<div class="gv-c-form-group ">
+        <label class="gv-c-form-group__label" for="full-name">
+          Full name
+        </label>
+        <input class="gv-c-form-group__control " id="full-name" name="full-name" value="&quot;/&gt;&lt;script&gt;document.location.href=&quot;http://example.com&quot;&lt;/script&gt;" type="text">
+      </div>`
+    )
+  })
+
   it('should render textarea with a value', () => {
     expectComponent(
       'form-group',
