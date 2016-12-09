f83972ec7c0ad37bf5aa4b0c7ba3ab4152ccf869
David Singleton <david@dsingleton.co.uk>
2016-12-09 09:31:51 +0000

    Allows setting of default value for form-group
    Would be used for setting default values or repopulating after
    a user submission.
    
    Adding this as an example to help with testing of escaping, as
    form values are a realistic place to expect unsafe input.

--- test/specs/components/form_group_spec.js
+++ test/specs/components/form_group_spec.js
@@ -17,4 +17,41 @@ describe('Form group component', function () {
       </div>`
     )
   })
+
+  it('should render with a value', () => {
+    expectComponent(
+      'form-group',
+      {
+        'id': 'full-name',
+        'name': 'full-name',
+        'label': 'Full name',
+        'value': 'My example name'
+      },
+      `<div class="gv-c-form-group ">
+        <label class="gv-c-form-group__label" for="full-name">
+          Full name
+        </label>
+        <input class="gv-c-form-group__control " id="full-name" name="full-name" value="My example name" type="text">
+      </div>`
+    )
+  })
+
+  it('should render textarea with a value', () => {
+    expectComponent(
+      'form-group',
+      {
+        'id': 'full-name',
+        'name': 'full-name',
+        'label': 'Full name',
+        'isTextarea': true,
+        'value': 'My example name'
+      },
+      `<div class="gv-c-form-group ">
+        <label class="gv-c-form-group__label" for="full-name">
+          Full name
+        </label>
+        <textarea class="gv-c-form-group__control " id="full-name" name="full-name" cols="30" rows="5">My example name</textarea>
+      </div>`
+    )
+  })
 })
