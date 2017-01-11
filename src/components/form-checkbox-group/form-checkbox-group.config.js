8d328b72fc70f078652ddc2cb4e975c3aff038cb
Gemma Leigh <hello@gemmaleigh.com>
2017-01-11 16:49:28 +0000

    Add config for a checkbox component

---
+++ src/components/form-checkbox-group/form-checkbox-group.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: 'Form checkbox group',
+  status: 'wip',
+  context: {
+    setup: {
+      initScript: 'var $blockLabels = $(".gv-c-form-custom input[type=\'checkbox\']"); new GOVUK.SelectionButtons($blockLabels);',
+      openWrapper: '<form>',
+      closeWrapper: '</form>'
+    },
+    id: 'contact',
+    name: 'contact-group',
+    legend: 'How do you want to be contacted?',
+    hint: '',
+    error: '',
+    checkboxGroup: [{
+      id: 'example-contact-by-email',
+      value: 'contact-by-email',
+      label: 'Email'
+    }, {
+      id: 'example-contact-by-phone',
+      value: 'contact-by-phone',
+      label: 'Phone'
+    }, {
+      id: 'example-contact-by-text',
+      value: 'contact-by-text',
+      label: 'Text'
+    }]
+  },
+  variants: [{
+    name: 'has hint',
+    context: {
+      hint: 'Hint text in here'
+    }
+  },
+  {
+    name: 'has error',
+    context: {
+      error: 'Error text in here'
+    }
+  },
+  {
+    name: 'has hint and error',
+    context: {
+      hint: 'Hint text in here',
+      error: 'Error text in here'
+    }
+  }],
+  arguments: ['id', 'name', 'legend', 'hint', 'error', 'checkboxGroup']
+}
