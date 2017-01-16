376bba14220e41910359989df33400c61a13304e
Gemma Leigh <hello@gemmaleigh.com>
2017-01-16 12:15:34 +0000

    Add the exclamation icon

--- src/components/notice/notice.spec.js
+++ src/components/notice/notice.spec.js
@@ -9,7 +9,7 @@ describe('Notice component', () => {
         'text': 'Legal text goes in here'
       },
       `<div class="gv-c-notice">
-        <i class="gv-c-notice__icon"><span class="gv-c-notice__icon-fallback-text">Warning</span></i>
+        <i class="gv-c-notice__icon gv-c-notice__icon--important"><span class="gv-c-notice__icon-fallback-text">Warning</span></i>
         <strong class="gv-c-notice__text">Legal text goes in here</strong>
       </div>`
     )
